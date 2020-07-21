e2d2dbe62bb9c72b851e885f2326a9717cbf6edd
Adam Wootton <adam@taplytics.com>
2020-07-21 00:26:08 -0400

    form

--- web/src/App.js
+++ web/src/App.js
@@ -2,41 +2,40 @@ import React, { useEffect, useState } from 'react';
 import { connect } from './services/web3'
 import { connect as connect3Box, getClient } from './services/3box'
 import { connect as connectIpfs } from './services/ipfs'
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const [loading, setLoading] = useState(true)
-  const [name, setName] = useState('')
+    const [loading, setLoading] = useState(true)
+    const [name, setName] = useState('')
+    const [profile, setProfile] = useState({})
 
-  useEffect(() => {
-      (async () => {
-        await connect()
-        await connect3Box()
-        await connectIpfs()
-        setName(await getClient().public.get('name'))
-        setLoading(false)
-      })()
-  }, [])
+    useEffect(() => {
+        (async () => {
+            await connect()
+            await connect3Box()
+            await connectIpfs()
+            const name = await getClient().public.get('name')
+            setProfile({...profile, name})
+            setLoading(false)
+        })()
+    }, [])
 
-  const saveName = async () => {
-    getClient().public.set('name', name)
-  }
+    const saveName = async () => {
+        getClient().public.set('name', name)
+    }
 
-  if (loading) {
-    return <div>Loading...</div>
-  }
+    if (loading) {
+        return <div>Loading...</div>
+    }
 
-  const box = getClient()
-
-  return (
-    <div className="App">
-      <h1>Your 3Box Name is {name || 'not set'}</h1>
-      <input placeholder={'Enter Your Name'} onChange={(e) => setName(e.target.value)} value={name}/>
-      <button onClick={saveName}>Change Name</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Your 3Box Name is {profile.name || 'not set'}</h1>
+            <input placeholder={'Enter Your Name'} onChange={(e) => setName(e.target.value)} value={name}/>
+            <button onClick={saveName}>Change Name</button>
+        </div>
+    );
 }
 
 export default App;
