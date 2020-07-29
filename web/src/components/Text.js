4797b1c23e16236b5fe6e0bf0192c21c2df67daa
Adam Wootton <adam@taplytics.com>
2020-07-28 22:49:08 -0400

    stuff

--- web/src/components/Text.js
+++
@@ -1,8 +0,0 @@
-import { FormField, TextInput} from 'grommet'
-import React from 'react'
-
-export default ({register, label, placeholder, ...props}) => {
-    return <FormField label={label}>
-        <TextInput ref={register} placeholder={placeholder} {...props}/>
-    </FormField>
-}
--- web/src/routes/NewProposal/NewProposal.js
+++ web/src/routes/NewProposal/NewProposal.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import { Header, Heading, Box, Button } from "grommet"
-import { useForm } from "react-hook-form"
-import Text from '../../components/Text'
+import React, {useState} from 'react'
+import {Header, Heading, Box, Button, Form, TextInput, FormField} from "grommet"
 
 export default () => {
-    const { register, handleSubmit } = useForm()
+    const [value, setValue] = React.useState({});
 
     const onSubmit = data => {
 
     }
 
     return <>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <Form
+            value={value}
+            onChange={nextValue => setValue(nextValue)}
+            onSubmit={({value}) => onSubmit(value)}>
             <Header background={'light-2'} pad={'medium'}>
                 <Heading>Create a New Proposal</Heading>
                 <Button primary label={'Submit'} type={'submit'}/>
             </Header>
             <Box width={'100%'} align={'center'} margin={'10px'}>
                 <Box width={'400px'} justify={'start'}>
-                        <Text label={'Name'} register={register}/>
+                    <FormField label={'Name'}>
+                        <TextInput ref={register} name={'name'}/>
+                    </FormField>
                 </Box>
             </Box>
-        </form>
+        </Form>
     </>
 }
