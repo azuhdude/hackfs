d77ee5343494b6df2bbc7be55fb3b49fa1f98d17
Imaad Umar <imaad.umar@tri-ad.global>
2020-08-06 00:58:23 +0900

    basic structre of the CLI without actually doing anything

--- upload_cli/src/index.js
+++ upload_cli/src/index.js
@@ -1,6 +1,55 @@
 const { Command, requiredOption } = require('commander');
 const program = new Command();
 
+function create_upload_receipt() {
+
+}
+
+function write_upload_receipt() {
+
+}
+
+function upload_file_to_ffs() {
+
+};
+
+function find_file_and_read_file() {
+
+}
+
+function parse_list_of_files() {
+
+}
+
+function file_data_mode() {
+    // parse train data list
+    // for list of file
+        // find file 
+        // upload file
+        // save data address
+
+    // parse test data list
+    // for list of file
+        // find file
+        // upload file
+        // save data address
+
+    // format data address json
+    // format data address json
+    // write data_upload_reciept_train_data
+    // write data_upload_receipt_test_data
+}
+
+function numerical_data_mode() {
+    // upload train data csv
+    // upload test data csv
+    // save data address
+    // save data address
+    // format data address json
+    // format data address json
+    // write data_upload_reciept_train_data
+    // write data_upload_receipt_test_data
+}
 
 program
     .command('upload')
