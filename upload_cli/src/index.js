2608ca1b5940cbf8caa336ba58a5c03f0d3b104a
Imaad Umar <imaad.umar@tri-ad.global>
2020-08-06 00:24:10 +0900

    fixing error with required option and option type

--- upload_cli/src/index.js
+++ upload_cli/src/index.js
@@ -1,24 +1,28 @@
-const { Command } = require('commander');
+const { Command, requiredOption } = require('commander');
 const program = new Command();
 
 
 program
+    .command('upload')
     .command('file_data')
-    .option('--train_data_list')
-    .option('--test_data_list')
+    .requiredOption('--train_data_list <string>')
+    .requiredOption('--test_data_list <string>')
     .action(function (args) {
         console.log('file_data')
-        console.log(args.traid_data_list)
+        console.log(args.train_data_list)
         console.log(args.test_data_list)
     })
     
 
 program
     .command('numerical_data')
-    .option('--train_data_file')
-    .option('--test_data_file')
+    .requiredOption('--train_data_file <string>')
+    .requiredOption('--test_data_file <string>')
     .action(function (args) {
         console.log('numerical_data')
         console.log(args.train_data_file)
         console.log(args.test_data_file)
     })
+
+
+program.parse(process.argv)
